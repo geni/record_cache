2c71deda99566aaf7a91cdccc8edf32c133344de
Scott Steadman <scott.steadman@geni.com>
2026-04-08 15:36:21 -0700

    use postgres user for database connection

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,7 +9,7 @@ RecordCache.config(:cache => CACHE)
 ActiveRecord::Base.establish_connection(
   :adapter  => "postgresql",
   :host     => ENV.fetch('DB_HOST', 'localhost'),
-  :username => `whoami`.chomp.tr('.', '_'),
+  :username => `postgres',
   :password => "",
   :database => "record_cache_test"
 )
